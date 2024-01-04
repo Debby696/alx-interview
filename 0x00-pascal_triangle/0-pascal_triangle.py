91489db3b2f3294c36455af7290778185677bd5c
Debby696 <idowuopeyemi696@gmail.com>
2024-01-04 16:16:36 +0000

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#usr/bin/python3
+'''returns a list of lists of integers reprsenting the Pascals triangle of n'''
+
+
+def pascal_triangle(n):
+    '''Function that return a pascal triangle'''
+    check_result = []
+    if type(n) is not int or n <= 0:
+        return check_result
+    result = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(result[i - 1][j - 1] + result[i - 1][j])
+        row.append(1)
+        result.append(row)
+    return result
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,5 @@
+ALX INTERVIEW
+
+This repository contains practice questions to likely interview questions.
+The objective is to help prepare for the real world so thatr by the time i am out of ALX, I am fuly equipped for the software engineering life.
+
